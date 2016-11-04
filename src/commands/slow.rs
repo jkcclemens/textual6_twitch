cda361ff67241784d05032cece8f6e7a080c2203
Kyle Clemens <me@kyleclemens.com>
2016-11-04 19:06:39 -0400

    refactor(slow): use match instead of if let

--- src/commands/slow.rs
+++ src/commands/slow.rs
@@ -15,21 +15,22 @@ impl HasName for Slow {
 
 impl HasEntryPoint for Slow {
   fn entry<'a>(&self, _: &CommandInfo, matches: &ArgMatches<'a>) {
-    if let Some(duration) = matches.value_of("duration") {
-      let duration = match duration.parse::<usize>() {
-        Ok(d) => d,
-        Err(e) => {
-          echo(&format!("Could not parse duration: {}", e));
-          return;
+    match matches.value_of("duration") {
+      None => command("slow", None),
+      Some(duration) => {
+        let duration = match duration.parse::<usize>() {
+          Ok(d) => d,
+          Err(e) => {
+            echo(&format!("Could not parse duration: {}", e));
+            return;
+          }
+        };
+        if duration == 0 {
+          command("slowoff", None);
+        } else {
+          command("slow", Some(&[&duration.to_string()]));
         }
-      };
-      if duration == 0 {
-        command("slowoff", None);
-      } else {
-        command("slow", Some(&[&duration.to_string()]));
       }
-      return;
-    };
-    command("slow", None);
+    }
   }
 }
