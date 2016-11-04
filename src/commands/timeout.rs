507f30ca6fbadb20324b73f2dfa1b61022cdee57
Kyle Clemens <me@kyleclemens.com>
2016-11-04 19:07:03 -0400

    refactor(timeout): extract who logic

--- src/commands/timeout.rs
+++ src/commands/timeout.rs
@@ -3,6 +3,8 @@ use clap::ArgMatches;
 use commands::{Command, HasEntryPoint, HasName};
 use {CommandInfo, echo, command};
 
+type TimeoutFunction = Box<Fn(&str, &ArgMatches) -> ()>;
+
 pub struct Timeout;
 
 impl Command for Timeout {}
@@ -15,24 +17,26 @@ impl HasName for Timeout {
 
 impl HasEntryPoint for Timeout {
   fn entry<'a>(&self, _: &CommandInfo, matches: &ArgMatches<'a>) {
-    if let Some(sub) = matches.subcommand_matches("add") {
-      add_timeout(sub);
+    let (func, sub) = if let Some(sub) = matches.subcommand_matches("add") {
+      (Box::new(add_timeout) as TimeoutFunction, sub)
     } else if let Some(sub) = matches.subcommand_matches("remove") {
-      remove_timeout(sub);
+      (Box::new(remove_timeout) as TimeoutFunction, sub)
     } else {
       echo("Unsupported timeout operation.");
-    }
+      return;
+    };
+    let who = match sub.value_of("who") {
+      Some(w) => w,
+      None => {
+        echo("No target for timeout.");
+        return;
+      }
+    };
+    func(who, sub);
   }
 }
 
-fn add_timeout(matches: &ArgMatches) {
-  let who = match matches.value_of("who") {
-    Some(w) => w,
-    None => {
-      echo("No target for timeout.");
-      return;
-    }
-  };
+fn add_timeout(who: &str, matches: &ArgMatches) {
   match matches.value_of("duration") {
     Some(d) => {
       match d.parse::<usize>() {
@@ -53,13 +57,6 @@ fn add_timeout(matches: &ArgMatches) {
   }
 }
 
-fn remove_timeout(matches: &ArgMatches) {
-  let who = match matches.value_of("who") {
-    Some(w) => w,
-    None => {
-      echo("No target for timeout.");
-      return;
-    }
-  };
+fn remove_timeout(who: &str, _: &ArgMatches) {
   command("untimeout", Some(&[who]));
 }
